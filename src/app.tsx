702717645c9859d200c9893c944b36adf8b9a8c1
Eli <elham@browse.ai>
2022-05-17 06:06:52 +0430

    feat(grommet): add grommet ui tools with themes

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {Grommet} from 'grommet'
+import themes from './themes.json'
 import Home from './pages/Home';
 
 function render() {
  ReactDOM.render(
     <React.StrictMode>
+      <Grommet theme={themes}>
       <Home/>
+      </Grommet>
     </React.StrictMode>,
     document.getElementById('app')
 );
